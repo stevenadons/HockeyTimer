008a7f8e409a1c8f33ef5475216a35779e2fd9ec
Steven <steven.adons@gmail.com>
2020-06-28 12:37:00 +0200

    fixed bug in GameTimePickers

--- HockeyTimer/GameTimePickers.swift
+++ HockeyTimer/GameTimePickers.swift
@@ -34,7 +34,17 @@ class GameTimePickers: UIView {
     
     // MARK: - Init
     
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        setup()
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        setup()
+    }
     
     private func setup() {
         
